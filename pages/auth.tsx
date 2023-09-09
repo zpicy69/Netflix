9dd72e013de16aa0929db9dd9ec6160357cde786
Abdalla <zpicy69@aamsdoumani@gmail.com>
2023-09-09 11:47:41 -0600

    final

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -82,6 +82,12 @@ const Auth = () => {
                     {variant === 'login' ? 'Sign In' : 'Sign Up'}
                 </button>
                 <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                    <div onClick={() => signIn('google', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                        <FcGoogle size={32} />
+                    </div>
+                    <div onClick={() => signIn('github', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                        <FaGithub size={32} />
+                    </div>
                 </div>
                 <p className="text-neutral-500 mt-12">
                     {variant === 'login' ? 'New to Netflix?' : 'Already have an account?'}
